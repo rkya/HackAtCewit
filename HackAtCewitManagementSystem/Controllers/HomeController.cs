51bf3038f9d2b283cef62c1c88a44699af22a28d
christopher.j.ryan <christopher.j.ryan@stonybrook.edu>
2018-09-03 14:34:25 -0400

    Updating branding for consistent look and feel with main cewit website-- including modified hover attributes on navigation bar, unified header layout partial. Updated website information with new dates. Updated website links to refer to file documents from last year, and included documents in webroot.

--- HackAtCewitManagementSystem/Controllers/HomeController.cs
+++ HackAtCewitManagementSystem/Controllers/HomeController.cs
@@ -12,20 +12,21 @@ namespace HackAtCewitManagementSystem.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Active = "Home";
             return View();
         }
 
         public IActionResult About()
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
-
+            ViewBag.Active = "About";
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            ViewBag.Active = "Contact";
             return View();
         }
 
@@ -37,28 +38,28 @@ namespace HackAtCewitManagementSystem.Controllers
         public IActionResult Faq()
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
-
+            ViewBag.Active = "Faq";
             return View();
         }
 
         public IActionResult Schedule()
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
-
+            ViewBag.Active = "Schedule";
             return View();
         }
 
         public IActionResult Video()
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
-
+            ViewBag.Active = "Video";
             return View();
         }
 
         public IActionResult Resources()
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
-
+            ViewBag.Active = "Resources";
             return View();
         }
     }
