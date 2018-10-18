2fc9fbb0159f2468a9f843bda16391e1d24d7d2b
Aishwarya Danoji <aishwarya.danoji@stonybrook.edu>
2018-10-17 23:16:54 -0400

    dynamic faq

---
+++ HackAtCewitManagementSystem/Models/Faq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HackAtCewitManagementSystem.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class Faq
+    {
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+
+    }
+}
