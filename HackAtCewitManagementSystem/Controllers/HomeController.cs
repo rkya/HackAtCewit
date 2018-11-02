d37406a8b034bb06159a4680eab5642704ddc909
rkya <rkarhadkar@cs.stonybrook.edu>
2018-11-02 13:53:51 -0400

    Added logic to send results in View and Json mode depending on the header content of the request

--- HackAtCewitManagementSystem/Controllers/HomeController.cs
+++ HackAtCewitManagementSystem/Controllers/HomeController.cs
@@ -43,20 +43,20 @@ namespace HackAtCewitManagementSystem.Controllers
             //return View();
             var model = new List<Faq>();
 
-            using (SqliteConnection conn =
-                new SqliteConnection("Data Source=test.db"))
+            using (SqliteConnection conn = new SqliteConnection("Data Source=test.db"))
             {
                 conn.Open();
 
-                SqliteCommand cmd = new SqliteCommand
-            ("SELECT * from Faq", conn);
+                SqliteCommand cmd = new SqliteCommand("SELECT * from Faq", conn);
 
                 SqliteDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
-                    var faq = new Faq();
-                    faq.Question = (string)rdr["Question"];
-                    faq.Answer = (string)rdr["Answer"];
+                    var faq = new Faq
+                    {
+                        Question = (string)rdr["Question"],
+                        Answer = (string)rdr["Answer"]
+                    };
 
                     model.Add(faq);
                 }
@@ -65,19 +65,17 @@ namespace HackAtCewitManagementSystem.Controllers
             return View(model);
         }
 
-        public IActionResult Schedule()
+        public IActionResult Schedule([FromHeader]string sendJson)
         {
             ViewData["Message"] = "Hack@CEWIT is the Center of Excellence in Wireless and Information Technology (CEWIT)'s interdisciplinary IoT-focused hackathon bringing students together for a two-day technical challenge over President's Day Weekend.";
             //ViewBag.Active = "Schedule";
             var model = new List<Schedule>();
 
-            using (SqliteConnection conn =
-                new SqliteConnection("Data Source=test.db"))
+            using (SqliteConnection conn = new SqliteConnection("Data Source=test.db"))
             {
                 conn.Open();
 
-                SqliteCommand cmd = new SqliteCommand
-            ("SELECT * from Schedule", conn);
+                SqliteCommand cmd = new SqliteCommand("SELECT * from Schedule", conn);
 
                 SqliteDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -92,7 +90,8 @@ namespace HackAtCewitManagementSystem.Controllers
                 }
             }
             ViewBag.Active = "Schedule";
-            return View(model);
+
+            return sendJson != null && sendJson.Equals("True") ? Json(model) : (IActionResult)View(model);
         }
 
         public IActionResult Video()
