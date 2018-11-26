cd079d90ba9fdeb7b191aa6ceba43cdcb2855997
rkya <rkarhadkar@cs.stonybrook.edu>
2018-11-25 23:39:51 -0500

    Leaderboard complete

--- HackAtCewitManagementSystem/Controllers/LeaderBoardController.cs
+++ HackAtCewitManagementSystem/Controllers/LeaderBoardController.cs
@@ -12,10 +12,10 @@ using Microsoft.Data.Sqlite;
 
 namespace HackAtCewitManagementSystem.Controllers
 {
-    [Authorize(Roles = "admin, participant")]
     public class LeaderBoardController : Controller
     {
         [Route("LeaderBoard")]
+        [Authorize(Roles = "admin, participant")]
         public IActionResult Index([FromHeader]string sendJson)
         {
             ViewBag.Active = "LeaderBoard";
@@ -23,6 +23,75 @@ namespace HackAtCewitManagementSystem.Controllers
             var board = LeaderBoardDBConnector.GetLeaderBoard(sqlString);
 
             return sendJson != null && sendJson.Equals("True") ? Json(board) : (IActionResult)View(board);
+        }
+
+        //[AcceptVerbs("POST", "GET")]
+        //[Route("LeaderBoard/Add/{id}")]
+        //[Authorize(Roles = "admin")]
+        //public IActionResult Add(LeaderBoard leaderBoardRow, string id)
+        //{
+        //    Console.WriteLine("------------------------------------------");
+        //    Console.WriteLine(leaderBoardRow.Username);
+        //    Console.WriteLine(leaderBoardRow.Score);
+        //    Console.WriteLine(id);
+        //    leaderBoardRow.Username = id;
+        //    LeaderBoardDBConnector.Create(leaderBoardRow);
+
+        //    return Redirect("/Home");
+        //}
+
+        //[HttpPost]
+        //[Route("LeaderBoard/Add")]
+        //[Authorize(Roles = "admin")]
+        //public IActionResult Add(LeaderBoard leaderBoardRow)
+        //{
+        //    Console.WriteLine("============================");
+        //    Console.WriteLine(leaderBoardRow.Username);
+        //    Console.WriteLine(leaderBoardRow.Score);
+        //    LeaderBoardDBConnector.Create(leaderBoardRow);
+
+        //    return Redirect("/Videos");
+        //}
+
+        [HttpGet]
+        [Route("LeaderBoard/Add")]
+        [Authorize(Roles = "admin")]
+        public IActionResult Add() 
+        {
+            return View(LeaderBoardDBConnector.GetUsersNotOnLeaderBoard());
+        }
+
+        [AcceptVerbs("POST", "PUT")]
+        [Route("LeaderBoard/Edit/{id}")]
+        [Authorize(Roles = "admin")]
+        public IActionResult Edit(LeaderBoard leaderBoard, string id)
+        {
+            leaderBoard.Username = id;
+            LeaderBoardDBConnector.Update(leaderBoard);
+
+            return Redirect("/LeaderBoard");
+        }
+
+        [HttpGet]
+        [Route("LeaderBoard/Edit/{id}")]
+        [Authorize(Roles = "admin")]
+        public IActionResult Edit(string id)
+        {
+            LeaderBoard user = LeaderBoardDBConnector.GetLeaderBoardRow(id);
+            Console.WriteLine(user.Username);
+            user.Username = id;
+            Console.WriteLine(user.Username);
+            return View(user);
+        }
+
+        [HttpGet]
+        [Route("LeaderBoard/Delete/{id}")]
+        [Authorize(Roles = "admin")]
+        public IActionResult Delete(string id)
+        {
+            Console.WriteLine(id);
+            LeaderBoardDBConnector.Delete(id);
+            return Redirect("/LeaderBoard");
         }
     }
 }
--- HackAtCewitManagementSystem/Utils/LeaderBoardDBConnector.cs
+++ HackAtCewitManagementSystem/Utils/LeaderBoardDBConnector.cs
@@ -28,11 +28,116 @@ namespace HackAtCewitManagementSystem.Utils
                     {
                         Username = rdr["Username"] == DBNull.Value ? "" : (string)rdr["Username"],
                         Score = rdr["Score"] == DBNull.Value ? -1 : (long)rdr["Score"],
-                        LastUpdated = rdr["LastUpdated"] == DBNull.Value ? "" : (string)rdr["LastUpdated"],
-                        Rank = rdr["Rank"] == DBNull.Value ? -1 : (long)rdr["Rank"]
+                        LastUpdated = rdr["LastUpdated"] == DBNull.Value ? "" : (string)rdr["LastUpdated"]
                     };
 
                     leaderBoard.Add(userPosition);
+                }
+            }
+
+            return leaderBoard;
+        }
+
+        public static LeaderBoard GetLeaderBoardRow(string username)
+        {
+            LeaderBoard leaderBoard = null;
+
+            using (SqliteConnection conn = new SqliteConnection(Constants.DATA_SOURCE))
+            {
+                conn.Open();
+
+                SqliteCommand cmd = new SqliteCommand("SELECT * FROM LeaderBoard WHERE Username = '" + username + "' ORDER BY Score DESC", conn);
+
+                SqliteDataReader rdr = cmd.ExecuteReader();
+
+
+                while (rdr.Read())
+                {
+                    leaderBoard = new LeaderBoard
+                    {
+                        Username = rdr["Username"] == DBNull.Value ? "" : (string)rdr["Username"],
+                        Score = rdr["Score"] == DBNull.Value ? -1 : (long)rdr["Score"],
+                        LastUpdated = rdr["LastUpdated"] == DBNull.Value ? "" : (string)rdr["LastUpdated"]
+                    };
+
+                }
+            }
+
+            return leaderBoard ?? new LeaderBoard();
+        }
+
+        public static bool Create(LeaderBoard leaderBoard)
+        {
+            return InsertOrUpdate(leaderBoard);
+        }
+
+        public static bool Update(LeaderBoard leaderBoard)
+        {
+            return InsertOrUpdate(leaderBoard);
+        }
+
+        private static bool InsertOrUpdate(LeaderBoard leaderBoard)
+        {
+            using (SqliteConnection conn = new SqliteConnection(Constants.DATA_SOURCE))
+            {
+
+                conn.Open();
+                SqliteCommand insertSQL = new SqliteCommand("INSERT OR REPLACE INTO LeaderBoard(Username, Score, LastUpdated) VALUES ('" + leaderBoard.Username + "', " + leaderBoard.Score + ", datetime('now'))", conn);
+
+                try
+                {
+                    insertSQL.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Delete(string username)
+        {
+            using (SqliteConnection conn = new SqliteConnection(Constants.DATA_SOURCE))
+            {
+                conn.Open();
+                SqliteCommand deleteSQL = new SqliteCommand("DELETE FROM LeaderBoard WHERE Username = '" + username + "'", conn);
+
+                try
+                {
+                    deleteSQL.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static List<LeaderBoard> GetUsersNotOnLeaderBoard()
+        {
+            List<LeaderBoard> leaderBoard = new List<LeaderBoard>();
+
+            using (SqliteConnection conn = new SqliteConnection(Constants.DATA_SOURCE))
+            {
+                conn.Open();
+
+                SqliteCommand cmd = new SqliteCommand("SELECT * FROM Checkin WHERE UserName NOT IN (SELECT DISTINCT Username FROM LeaderBoard)", conn);
+
+                SqliteDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var leaderBoardRow = new LeaderBoard
+                    {
+                        Username = rdr["Username"] == DBNull.Value ? "" : (string)rdr["Username"]
+                    };
+
+                    leaderBoard.Add(leaderBoardRow);
                 }
             }
 
